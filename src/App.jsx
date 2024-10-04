e4873922db927d180193a69a32d3c3e36a55aff6
Dinesh18042001 <dinesh@1804200182@gmail.com>
2024-10-04 16:57:23 +0530

    update some page

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,44 @@
 import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
-import { ScrollRestoration } from "react-router-dom";
+import ScrollToTop from './ScrollToTop';  
 import "./App.css";
-import { Header } from "./Header/Header";
+import NavTwo from './NavTwo/NavTwo';  
+import { Footer } from "./Footer/Footer";
 import { BannerHome } from "./Banner/BannerHome";
 import { Companies } from "./Company-logo/Companies";
 import { Idea } from "./idea/Idea";
 import { News } from "./News/News";
 import { Services } from "./Services/Services";
 import { Certifications } from "./Certifications/Certifications";
-import  {Case}  from "./Case/Case";
+import { Case } from "./Case/Case";
 import VisionIndustries from "./VisionIndustries/VisionIndustries";
 import Clients from "./Clients/Clients";
-import Blogs from "./Blogs/Blogs.jsx";
-import ContactForm from "./ContactForm/ContactForm";
-import { Footer } from "./Footer/Footer";
-import {About} from "./About/About"
+import Blogs from './Blogs/Blogs.jsx';
+import ContactForm from './ContactForm/ContactForm';
+import { About } from './About/About';
 import Assistant from './Assistant/Assistant';
 import BlogPage from './BlogPage/BlogPage.jsx';
 import Careers from './Careers/Careers.jsx';
-import NavTwo from './NavTwo/NavTwo.jsx';
 import { ContactPage } from './ContactPage/ContactPage.jsx';
 import WebDevelopment from './ServicesPage/WebDevelopment/WebDevelopment.jsx';
 import VirtualAssistant from './ServicesPage/VirtualAssistant/VirtualAssistant.jsx';
 import AppDevelopment from './ServicesPage/AppDevelopment/AppDevelopment.jsx';
 import DigitalMarketing from './ServicesPage/DigitalMarketing/DigitalMarketing.jsx';
 import CloudServices from './ServicesPage/CloudServices/CloudServices.jsx';
-import ScrollToTop from './ScrollToTop.js';
 import MobileApp from './BlogDetails/MobileApp.jsx';
 import Digital from './BlogDetails/Digital.jsx';
 import WebDev from './BlogDetails/WebDev.jsx';
 import VirtualAss from './BlogDetails/VirtualAss.jsx';
 
-const Layout =() => {
+const Layout = () => {
   return (
     <>
-      {/* <Header /> */}
-      <NavTwo/>
-      <ScrollToTop/>
-      <Outlet /> 
+      
+      <NavTwo />
+    
+      <ScrollToTop />
+    
+      <Outlet />
+
       <Footer />
     </>
   );
@@ -59,24 +60,22 @@ function App() {
         { path: "/clients", element: <Clients /> },
         { path: "/blogs", element: <Blogs /> },
         { path: "/contact", element: <ContactForm /> },
-        { path: "/about", element: <About/> },
-        { path: "/assistant", element: <Assistant/> },
-        { path: "/blogs", element: <Blogs/> },
-        { path: "/blogspage", element: <BlogPage/> },
-        { path: "/careers", element: <Careers/> },
-        { path: "/contactpage", element: <ContactPage/>},
-        { path: "/webdevelopment", element: <WebDevelopment/>},
-        { path: "/virtualassistant", element: <VirtualAssistant/>},
-        { path: "/appdevelopment", element: <AppDevelopment/>},
-        { path: "/digitalmarketing", element: <DigitalMarketing/>},
-        { path: "/cloudservices", element: <CloudServices/>},
-        { path: "/mobileappblog", element: <MobileApp/>},
-        { path: "/digitalblog", element: <Digital/>},
-        { path: "/webdevblog", element: <WebDev/>},
-        { path: "/virtualassblog", element: <VirtualAss/>},
-
-      ]
-    }
+        { path: "/about", element: <About /> },
+        { path: "/assistant", element: <Assistant /> },
+        { path: "/blogspage", element: <BlogPage /> },
+        { path: "/careers", element: <Careers /> },
+        { path: "/contactpage", element: <ContactPage /> },
+        { path: "/webdevelopment", element: <WebDevelopment /> },
+        { path: "/virtualassistant", element: <VirtualAssistant /> },
+        { path: "/appdevelopment", element: <AppDevelopment /> },
+        { path: "/digitalmarketing", element: <DigitalMarketing /> },
+        { path: "/cloudservices", element: <CloudServices /> },
+        { path: "/mobileappblog", element: <MobileApp /> },
+        { path: "/digitalblog", element: <Digital /> },
+        { path: "/webdevblog", element: <WebDev /> },
+        { path: "/virtualassblog", element: <VirtualAss /> },
+      ],
+    },
   ]);
 
   return (
